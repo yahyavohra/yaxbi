b8d9fa24365b2d128723670298a6f5a901d9d8c6
Arsalan Khan <yahya2vohra@gmail.com>
2024-04-19 21:32:49 +0400

    pushed metas

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,9 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <meta name="google-site-verification" content="jRpOmOLhzaWrust7ul5nfr4gF8-hRE32GQAYY25aGtg" />
+        <title>Yaxbi - Transforming Ideas into Innovative Tech Solutions</title>
+        <meta name="description" content="Yaxbi specializes in tech solutions for business growth, offering expertise in MVP development and digital transformation. Unlock your business potential with Yaxbi. Discover how Yaxbi can transform your business with our comprehensive tech solutions. From MVP development to enterprise digital transformation, we have you covered."/>
+        <meta name="keywords" content="Yaxbi, tech solutions, MVP development, digital transformation, startups, scaleups, enterprise, data engineering, cyber security, quality analysis"/>
         <link rel="icon" type="image/svg+xml" href="/images/favicon.svg"/>
         <link rel="icon" type="image/png" href="/images/favicon.png"/>
       </Head>
