ead63047ed144a5ddfd052b4ad7cc3460a1fd4cc
Arsalan Khan <yahya2vohra@gmail.com>
2024-05-06 14:33:32 +0400

    fixd

--- src/components/contactUs/index.js
+++ src/components/contactUs/index.js
@@ -110,7 +110,7 @@ function contactUs() {
   allow="geolocation; microphone; camera; fullscreen"
   src="https://form.jotform.com/240912269825461"
   frameBorder={0}
-  style={{ minWidth: "100%", maxWidth: "100%", height: '800px', border: "none" }}
+  style={{ minWidth: "100%", maxWidth: "100%", height: '880px', border: "none" }}
   scrolling="no"
 />
         </div>
